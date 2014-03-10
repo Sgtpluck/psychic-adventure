21ea08b66b21fb9c91dd43592b94caa26020cacc
Davida Marion <davidamarion@gmail.com>
2014-03-10 13:58:19 -0700

    changes name

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -4,7 +4,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1;
 
 
-  data.buffer.push("<div class='container'>\n  <h1>My Ember App</h1>\n  <p class='lead'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>\n  <hr>\n  ");
+  data.buffer.push("<div class='container'>\n  <h1>PSYCHE!</h1>\n  <p class='lead'>Top-rated futures at discount prices!</p>\n  <hr>\n  ");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n  \n  <div class='text-muted'><small>Foot</small></div>\n</div>\n");
