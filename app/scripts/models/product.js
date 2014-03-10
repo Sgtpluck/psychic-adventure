a3361992c22ca07a6cc016ad74bb5b02998ac23d
Davida Marion <davidamarion@gmail.com>
2014-03-10 14:40:03 -0700

    products showing up! in a list! with prices nicely formatted into dollars!

---
+++ app/scripts/models/product.js
@@ -0,0 +1,25 @@
+App.Product = DS.Model.extend({
+  name: DS.attr("string"),
+  description: DS.attr("string"),
+  price: DS.attr("integer"),
+  image: DS.attr("string"),
+  avatar: DS.attr("string")
+});
+
+App.Product.FIXTURES = [
+  {
+    id: 1,
+    name: "Financial",
+    description: "Are you worried about your financial future? Do you want to know what is in store for you, but are you too smart to spend a lot of money trying to figure it out? Let us help ease your mind by telling you all about the fortunes that are about to fall into your lap, for a low, low price.",
+    price: 100000,
+    image: "http://upload.wikimedia.org/wikipedia/commons/f/f9/Money_Cash.jpg",
+    avatar: "http://petswithstyleboutique.com/images/thumbnails/DollarSignSV.jpg"
+  }, {
+    id: 2,
+    name: "Romantic",
+    description: "Have you been looking for love in all the wrong places? We can help you pinpoint exactly where you've been going wrong in all your romantic endeavers. It'll cost you, but whoever said that love came cheap?",
+    price: 500000,
+    image: "http://www.thebuccaneer.com/photos09/09-romance-2013-760.jpg",
+    avatar: "http://www.acclaimclipart.com/gifart_topics/topic_Misc/tn_heart7-Love%20and%20Romance-Love%20and%20Romance-9_0303-0800-1451.gif"
+  }
+];
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -4,10 +4,54 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1;
 
 
-  data.buffer.push("<div class='container'>\n  <h1>PSYCHE!</h1>\n  <p class='lead'>Top-rated futures at discount prices!</p>\n  <hr>\n  ");
+  data.buffer.push("<div class='container'>\n  <h1>PSYCHE!</h1>\n  <p class='lead'>Top-rated predictions at discount prices!</p>\n  <hr>\n  ");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n  \n  <div class='text-muted'><small>Foot</small></div>\n</div>\n");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["home"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<h1> GIT YR FUTURES HERE </h1>");
+  
+});
+
+Ember.TEMPLATES["products"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+function program1(depth0,data) {
+  
+  var buffer = '', stack1, helper, options;
+  data.buffer.push("\n      <li> ");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "products", options) : helperMissing.call(depth0, "link-to", "products", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n        $");
+  data.buffer.push(escapeExpression((helper = helpers['format-price'] || (depth0 && depth0['format-price']),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "price", options) : helperMissing.call(depth0, "format-price", "price", options))));
+  data.buffer.push("\n      </li>\n      ");
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push(" \n        ");
+  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n        ");
+  return buffer;
+  }
+
+  data.buffer.push("<h1>All my products!</h1>\n<div class='row'>\n  <div class='col-lg-2'>\n    <h4>All Products</h4>\n     <ul class='list-unstyled'>\n      ");
+  stack1 = helpers.each.call(depth0, {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[],types:[],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </ul>\n  </div>\n  <div class='col-lg-10'>\n   Product View Goes Here\n  </div>\n</div>");
   return buffer;
   
 });
