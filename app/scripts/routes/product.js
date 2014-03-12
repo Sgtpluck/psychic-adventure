bae7d0d033bf1062d6dd49a2f4caa97ab99a85f4
Davida Marion <davidamarion@gmail.com>
2014-03-12 13:46:52 -0700

    apparently that breaks too many promises

--- app/scripts/routes/product.js
+++ app/scripts/routes/product.js
@@ -3,21 +3,10 @@ App.ProductRoute = Ember.Route.extend({
     addToCart: function(unique_prod) {
       var theCart = this.controllerFor('cart').get('model');
       var newItem = this.store.createRecord('item', { cart: theCart, product: unique_prod, quantity: 1 });
-      theCart.get("items").then(function(items) {
-        console.log()
-        console.log(items.get('item'))
-        console.log(newItem.get('product').get("name"))
-        itemIndex = items.indexOf(newItem.get('product'));
-        console.log(itemIndex)
-        // })
-        // if (itemIndex == -1) {
-        //   newItem.save();
-        //   console.log(newItem)
-        //   items.pushObject(newItem);
-        //   }
-        // else {
-        //   console.log('hello')
-        // }
+      newItem.save();
+      console.log(newItem)
+      theCart.get("items").then(function(items) { 
+        items.pushObject(newItem);
       }
     )}
   },
@@ -29,15 +18,3 @@ App.ProductRoute = Ember.Route.extend({
     controller.set('model', model);
   },
 });
-
-  // theCart.get("items").then(function(items) {
-  //       item-index = items.indexOf(newItem)
-  //       if (item-index === -1) {
-  //         newItem.save();
-  //         items.pushObject(newItem);
-  //       }
-  //       else {
-  //         cart-item = items[item-index];
-  //         console.log(cart-item);
-  //       }
-  //     })
