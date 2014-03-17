9fdaf263f81aa4f8e82c252b0626847ae822ad3a
Davida Marion <davidamarion@gmail.com>
2014-03-17 14:53:30 -0700

    small addition to confirmation page

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -146,10 +146,13 @@ function program3(depth0,data) {
 Ember.TEMPLATES["confirmation"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  
+  var buffer = '', stack1;
 
 
-  data.buffer.push("THIS IS CONFIRMED");
+  data.buffer.push("THIS IS CONFIRMED\nTHANKS ");
+  stack1 = helpers._triageMustache.call(depth0, "customerName", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  return buffer;
   
 });
 
