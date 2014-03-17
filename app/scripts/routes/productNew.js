150b0296f81ff86fdcc0f478550c63529604aa5b
Davida Marion <davidamarion@gmail.com>
2014-03-17 10:42:43 -0700

    makes the proxy object code muuuuuch simpler

--- app/scripts/routes/productNew.js
+++ app/scripts/routes/productNew.js
@@ -1,25 +1,35 @@
 App.ProductNewRoute = Ember.Route.extend({
   model: function () {
-    return proxy = Ember.ObjectProxy.create({
-      name: 'this is a proxy',
-      description: null,
-      price: null,
-      avatar: null,
-      image: null 
-    });
+    return {};
+    // proxy = Ember.ObjectProxy.create({
+    //   name: 'this is a proxy',
+    //   description: null,
+    //   price: null,
+    //   avatar: null,
+    //   image: null 
+    // });
     // return this.store.createRecord("product")
   },
   actions: {
     createProduct: function (proxy) {
-      newProduct = this.store.createRecord("product", {
-          name: proxy.get('name'),
-          description: proxy.get('description'),
-          price: proxy.get('price'),
-          avatar: proxy.get('avatar'),
-          image: proxy.get('image')
-        })
-      newProduct.save;
-      this.transitionTo("admin");
+      newProduct = this.store.createRecord("product", proxy);
+      // {
+      //     name: proxy.get('name'),
+      //     description: proxy.get('description'),
+      //     price: proxy.get('price'),
+      //     avatar: proxy.get('avatar'),
+      //     image: proxy.get('image')
+      //   })
+      var self = this
+      newProduct.save().then(
+        function () {
+          self.transitionTo("admin");
+        },
+        function (error) {
+          alert(error.responseText);
+          newProduct.deleteRecord();
+        }
+        );
     }
   }
 });
