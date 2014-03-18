2665d07d0e639d27125523a0fa80704631d61d28
Davida Marion <davidamarion@gmail.com>
2014-03-18 14:39:59 -0700

    creates a cart!

--- app/scripts/routes/application.js
+++ app/scripts/routes/application.js
@@ -1,16 +1,14 @@
 App.ApplicationRoute = Ember.Route.extend({
-  // localStorage.removeItem("cartId");
   beforeModel: function () {
+ 
     if (typeof(localStorage.cartId) === "undefined") {
-      var cart = this.store.createRecord("cart", {})
-      cart.save().then(
-        function (cart) {
-      localStorage.cartId = cart.get("id");
+      var cart = this.store.createRecord("cart")
+      cart.save().then(function (cart) {
+        localStorage.cartId = cart.id;
     });
     }
   },
   model: function () {
-    // return this.store.createRecord("cart", {});
     return this.store.find("cart", localStorage.cartId);
   }
 });
